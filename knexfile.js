5dd7b9a97192d9a9b0d78109efbfc6a5d06e42c3
Michael Perez <mperez6473@gmail.com>
2020-06-15 14:14:24 -0500

    Looked over the guided project and picked some bits off the knex file. Added a migration and a connection file.

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
-// Update with your config settings.
+require("dotenv").config();
 
+const pgConnection =
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
 module.exports = {
     development: {
         client: "sqlite3",
@@ -38,18 +40,17 @@ module.exports = {
     },
 
     production: {
-        client: "postgresql",
-        connection: {
-            database: "my_db",
-            user: "username",
-            password: "password",
-        },
+        client: "pg",
+        connection: pgConnection,
         pool: {
             min: 2,
             max: 10,
         },
         migrations: {
-            tableName: "knex_migrations",
+            directory: "./database/migrations",
+        },
+        seeds: {
+            directory: "./database/seeds",
         },
     },
 };
